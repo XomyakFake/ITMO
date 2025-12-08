e964aee52fb6a0e8c15f9def31c644cd3b2be6d0
XomyakFake <107266440+XomyakFake@users.noreply.github.com>
2025-12-09 01:22:38 +0300

    Add files via upload

---
+++ 1 year/Информатика/Labs/Lab4/Informatics_Lab4_Task3.py
@@ -0,0 +1,11 @@
+import configparser
+import json
+
+def bibl():
+    config = configparser.ConfigParser() 
+    config.read("Inflabs/lab4/schedule.ini", encoding="utf-8")
+    schedule = {section: dict(config[section]) for section in config.sections()} #section - секция файла, dict(config[section]) - внутринности секции
+
+    with open("Inflabs/lab4/schedule.json", "w", encoding="utf-8") as f:
+        json.dump(schedule,f,ensure_ascii=False, indent = 2)
+
